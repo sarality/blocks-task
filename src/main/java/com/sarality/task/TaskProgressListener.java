11a3a6f5b9aada84a16a05da57b1315f4289e6ea
Abhideep Singh <abhi@dothat.in>
2016-12-14 15:55:12 +0530

    Progress Listener takes in a single argument as the Progress

--- src/main/java/com/sarality/task/TaskProgressListener.java
+++ src/main/java/com/sarality/task/TaskProgressListener.java
@@ -3,10 +3,9 @@ package com.sarality.task;
 /**
  * Interface for classes that need to be notified on the progress of a Async task
  *
- * @author Satya@ (Satya Puniani)
+ * @author abhideep@ (Abhideep Singh)
  */
-public interface TaskProgressListener {
+public interface TaskProgressListener<P> {
 
-  void onProgress(int maxProgress, int progress);
-
+  void onProgressUpdate(P progress);
 }
